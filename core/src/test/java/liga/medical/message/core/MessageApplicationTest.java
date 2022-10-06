8ae6f253df012e73e83a74fe4b8e1cfdebb0df78
Matevosyan Karen <mr.mgo@yandex.ru>
2022-10-06 18:05:17 +0300

    Init structure

---
+++ core/src/test/java/liga/medical/message/core/MessageApplicationTest.java
@@ -0,0 +1,13 @@
+package liga.medical.message.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class MessageApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
