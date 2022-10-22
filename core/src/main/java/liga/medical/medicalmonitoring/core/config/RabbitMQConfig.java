19344e9b648a749d46e077492e69c8828e6108c7
Matevosyan Karen <mr.mgo@yandex.ru>
2022-10-22 23:46:06 +0300

    Update bean connection

--- core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitMQConfig.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitMQConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+        return new CachingConnectionFactory("rabbitmq");
     }
 
     @Bean
