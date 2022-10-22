b12411e76a0d5a0131fb40c94f3bacaaa2568df8
Matevosyan Karen <mr.mgo@yandex.ru>
2022-10-23 00:07:32 +0300

    Refactoring bean conection and update application.yaml

--- core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitMQConfig.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/config/RabbitMQConfig.java
@@ -6,14 +6,18 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("rabbitmq");
+        return new CachingConnectionFactory(host);
     }
 
     @Bean
